6c3d4d431f010379a0829828882740ffafff912c
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-11-22 12:09:26 -0500

    Update runaction.sh
    arrange sh

--- runaction.sh
+++ runaction.sh
@@ -4,6 +4,9 @@ pwd
 echo "$GITHUB_WORKSPACE"
 cd "$GITHUB_WORKSPACE" || exit 1
 ls 
-chmod +x /Scripts/make_file.sh
-sh ./Scripts/make_file.sh
+cd Scripts
+ls
+chmod +x make_file.sh
+sh make_file.sh
+
 echo "test shell script run"
